2de982ab3c15a217bf8497caba549626d194610d
じえっる〔JL_.〕#1606 <konelinx@posteo.net>
2020-09-03 17:05:39 +0200

    LICENCE

--- index.cc
+++ index.cc
@@ -1,3 +1,20 @@
+/* Copyright (C) 2020 KoneLinx
+   This file is part of wordexp.js.
+   wordexp.js is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 3.0 of the License, or (at your option) any later version.
+   wordexp.js is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   You should have received a copy of the GNU Lesser General Public
+   License along with the wordexp.js repository; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+/* Copyright (C) 1991-2020 Free Software Foundation, Inc.
+   Sourced from the GNU C Library.  */
+
 
 #include "./wordexp.h"
 
