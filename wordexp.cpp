d34aebe13b70e504280906c86a37e23fdba40c9e
じえっる〔JL_.〕#1606 <konelinx@posteo.net>
2020-09-03 16:44:18 +0200

    LICENCE
    LGPL-3.0-or-later

--- wordexp.cpp
+++ wordexp.cpp
@@ -1,19 +1,22 @@
-/* Copyright (C) 1991-2020 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
+/* Copyright (C) 2020 KoneLinx
+   This file is part of wordexp.js.
 
-   The GNU C Library is free software; you can redistribute it and/or
+   wordexp.js is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
+   version 3.0 of the License, or (at your option) any later version.
 
-   The GNU C Library is distributed in the hope that it will be useful,
+   wordexp.js is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
+   General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
+   License along with the wordexp.js repository; if not, see
    <https://www.gnu.org/licenses/>.  */
+
+/* Copyright (C) 1991-2020 Free Software Foundation, Inc.
+   Sourced from the GNU C Library.  */
 
 
 #include "wordexp.h"
